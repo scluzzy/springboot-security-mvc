5b9af9791503aded33929ff61943de1b706cb1e1
Abdulrahman Syed <3004sar2001@gmail.com>
2024-01-20 21:22:50 +0300

    Add app

---
+++ spring-mvc-security-demo/src/main/java/com/scluzzy/securitydemo/securitydemoApplication.java
@@ -0,0 +1,13 @@
+package com.scluzzy.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class securitydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(securitydemoApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-security-demo/src/test/java/com/scluzzy/securitydemo/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scluzzy.securitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
